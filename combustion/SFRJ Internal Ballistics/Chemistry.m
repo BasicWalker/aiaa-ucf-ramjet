cb13114a90396c2a7df010d6d817dc0c0dd3a464
BasicWalker <armalysamer1@gmail.com>
2021-03-08 23:58:15 -0500

    updated chemistry model to a class with methods. now includes equivalence ratio into AFT calculation

--- combustion/SFRJ Internal Ballistics/Chemistry.m
+++ combustion/SFRJ Internal Ballistics/Chemistry.m
@@ -16,172 +16,159 @@
 % Air Chemical Formula: 0.79N2 0.21O2
 % Reaction:             H2O CO2 N2
 % ----------------------------------------------------------------------- %
-function [phi, gamma_nzl, T_AFT] = Chemistry(f)
-% ----------------- Initialize Values ----------------- % 
-f_yield = f;                                                        % Actual fuel to air ratio     
-MW_ABS  = 12.011*3.85 + 1.008*4.85 + 14.007*0.43;                   % Molecular Weight of ABS
-MW_Air  = 28.9647;                                                  % Molecular Weight of Air
-MW_N2   = 14.007*2;                                                 % Molecular Weight of N2
-MW_O2   = 15.999*2;                                                 % Molecular Weight of O2
-MW_CO2  = 12.011 + 2*15.999;                                        % Molecular Weight of CO2
-
-% ---------------------- Stoichiometric Combustion ---------------------- % 
-% C3.85 H4.85 N0.43 + (A)(0.71N2 + 0.2102) -> (B)H2O + (D)CO2 + (E)N2
-% 3.85 = D
-% 4.85 = 2B
-% 0.43 + A*2*0.71 = 2E
-% A*0.21*2 = B + 2D
-% ----------------------------------------------------------------------- % 
-D = 3.85;                                                           % Systems of equations
-B = 4.85/2;                                                         % Systems of equations
-A = (B + 2*D)/(0.21*2);                                             % Systems of equations
-E = (0.43 + A*2*0.71)/2;                                            % Systems of equations
-mol_st = [A B D E];                                                 % Molar Matrix     
-mf_st = MW_ABS;                                                     % Mass of ABS (kg)
-ma_st = mol_st(1)*MW_Air;                                           % Mass of Air (kg)
-f_st = mf_st/ma_st;                                                 % Stoichiometric fuel to air ratio
-%fprintf('\nCHN + %.2f(0.71N2 + 0.21O2) -> %.2fH2O + %.2fCO2 + %.2fN2\n', mol_st(1),mol_st(2),mol_st(3),mol_st(4));
-
-phi = f_yield/f_st;
-
-if(phi == 1)                            %  Fuel lean Combustion 
+classdef Chemistry
+    properties
+        fuel_type
+        w
+        x
+        y
+        z
+        Cp_Fuel
+        hf_Fuel
+        MW_Fuel
+        MW_Air
+        MW_N2
+        MW_O2
+        MW_CO2
+        T_STP
+        T_air
+        T_ABS
+        CpMat
+        hfMat
+        T_mat
+        mol_st
+        mf_st
+        ma_st
+        f_st
         
-        T_AFT = 3189.37;                % Adiabatic Flame Temperature (K)
-        gamma_nzl = 1.3845;             % gamma at nozzle throat
+    end
+    methods
+        function init = Chemistry(fuel_type)
+            if ~exist('fuel_type','var')
+                % fuel type defaulted to ABS
+                init.fuel_type = 'ABS';
+                init.w = 3.85 ;  % fuel Carbon coefficient
+                init.x = 4.85;  % fuel Hydrogen coefficient
+                init.y = 0;  % fuel Oxygen coefficient
+                init.z = 0.43;  % fuel Nitrogen coefficient
+                init.Cp_Fuel = 29.67;  % molar specific heat  <kJ/kmol*K>
+                init.hf_Fuel = 62628;     % molar enthalpy of formation <kJ/kmol>
+            end
+            init.MW_Fuel = 12.011*init.w + 1.008*init.x + 15.9995*init.y + 14.007*init.z;                % Molecular Weight of Fuel
+            init.MW_Air  = 28.9647;                                                  % Molecular Weight of Air
+            init.MW_N2   = 14.007*2;                                                 % Molecular Weight of N2
+            init.MW_O2   = 15.999*2;                                                 % Molecular Weight of O2
+            init.MW_CO2  = 12.011 + 2*15.999;                                        % Molecular Weight of CO2
+            init.T_STP = 298;    % standard temp <K>
+            init.T_air = 475;    % <K>
+            init.T_ABS = init.T_STP;  % <K>
+            
+            hf_air = 0;         % molar enthalpy of formation <kJ/kmol>
+            hf_CO2 = -393520;   % molar enthalpy of formation <kJ/kmol>
+            hf_H2O = -241820;   % molar enthalpy of formation <kJ/kmol>
+            hf_N2  = 0;          % molar enthalpy of formation <kJ/kmol>
+            hf_O2  = 0; 
+            
+            Cp_air = 29.67;  % molar specific heat  <kJ/kmol*K>
+            Cp_CO2 = 37.22;  % molar specific heat  <kJ/kmol*K>
+            Cp_H2O = 36.57;  % molar specific heat  <kJ/kmol*K>
+            Cp_N2  = 29.02;   % molar specific heat  <kJ/kmol*K>
+            Cp_O2  = 29.376;  % molar specific heat  <kJ/kmol*K> 
+            
+            init.CpMat = [init.Cp_Fuel Cp_air Cp_H2O Cp_CO2 Cp_N2 Cp_O2 init.Cp_Fuel]';
+            init.hfMat = [init.hf_Fuel hf_air hf_H2O hf_CO2 hf_N2 hf_O2 init.hf_Fuel]';
+            
+            % runs stoichiometric coefficients
+            % ---------------------- Stoichiometric Combustion ---------------------- % 
+            % Cw Hx Ny + (A)(0.71N2 + 0.2102) -> (B)H2O + (D)CO2 + (E)N2
+            % 3.85 = D
+            % 4.85 = 2B
+            % 0.43 + A*2*0.71 = 2E
+            % A*0.21*2 = B + 2D
+            % ----------------------------------------------------------------------- % 
+            %    a     b  c  d
+            A = [0     0 -1  0;...  % carbon
+                 0    -2  0  0;...  % hydrogen
+                 0.42 -1 -2  0;...  % oxygen
+                 1.58  0  0 -2];    % nitrogen
+            B = [-init.w;-init.x;-init.y;-init.z];
+
+            init.mol_st = linsolve(A,B); 
+            init.mf_st = init.MW_Fuel;  % Mass of fuel (kg)
+            init.ma_st = init.mol_st(1)*init.MW_Air;  % Mass of Air (kg)
+            init.f_st = init.mf_st/init.ma_st;  % Stoichiometric fuel to air ratio            
+            
+        end
         
-    elseif(phi < 1)                    
-% ------------------------ Fuel lean Combustion ------------------------- % 
-% C3.85 H4.85 N0.43 + (1/phi)(A)(0.71N2 + 0.2102) -> (B)H2O + (D)CO2 + (E)N2 + (F)O2
-% C: 3.85 = D
-% H: 4.85 = 2B
-% N: 0.43 + A*2*0.71 = 2E
-% O: A*0.21*2 = B + 2D
-% ----------------------------------------------------------------------- % 
+        function T_AFT = AFT(self, mol_mat, T_mat)
+            T_AFT = (sum(mol_mat.*self.hfMat) + sum(mol_mat.*self.CpMat.*(T_mat-self.T_STP)))...
+                / abs(sum(mol_mat(3:end).*self.CpMat(3:end)));
+        end
         
-        T_AFT = 3189.37;            % Adiabatic Flame Temperature (K)
-        gamma_nzl = 1.3845;         % gamma at nozzle throat
-    
-    else                            
-% ------------------------ Fuel Rich Combustion ------------------------- % 
-% C3.85 H4.85 N0.43 + (1/phi)(A)(0.71N2 + 0.2102) -> (B)H2O + (D)CO2 + (E)N2 + (F)C3.85 H4.85 N0.43
-% C: 3.85 = D + 3.85F
-% H: 4.85 = 2B + 4.85F
-% N: 0.43 + 2*0.79*A*(1/phi) = 2E + 0.43F
-% O: 2*0.21*A*(1/phi) = B + 2D
-% ----------------------------------------------------------------------- % 
-phi = 1.3;
-molair = mol_st(1);
- syms B D E F
- eqn1 = D + 3.85*F == 3.85;
- eqn2 = 2*B + 4.85*F == 4.85;
- eqn3 = 2*E + 0.43*F == 0.43 + (2*0.79*molair)/phi;
- eqn4 = B + 2*D == (2*0.21*molair)/phi;
- [LeftMatrix,RightMatrix] = equationsToMatrix([eqn1, eqn2, eqn3, eqn4],[B, D, E, F]);
- mol_fr = vpa(linsolve(LeftMatrix,RightMatrix))
+        function [phi, T_AFT, mol_mat] = phiSolver(self, f, T_air)
+            % ----------------- Initialize Values ----------------- % 
+            f_yield = f;                                                        % Actual fuel to air ratio     
+            phi = f_yield/self.f_st;
 
-F = (15.4/20.25) - ((15.4 * 0.21 * molair * (1/phi))/(20.25 * 3.85)) - (4.85/20.25);
-D = 3.85 - 3.85*F;
-B = 2*0.21*molair*(1/phi) - 2*D;
-E = (0.43 - 0.43*F + 2*0.79*molair*(1/phi))/2;
+            if(phi == 1)  
+            % ------------------------ Stoichiometric Combustion -------------------- % 
+            % C3.85 H4.85 N0.43 + (1/phi)(A)(0.71N2 + 0.2102) -> (B)H2O + (D)CO2 + (E)N2 + (F)O2
+            % ----------------------------------------------------------------------- %     
+                air_coeff = self.mol_st(1)/phi;
+                %     b  d  e  f g
+                A = [ 0 -1  0  0 0;...  % carbon
+                     -2  0  0  0 0;...  % hydrogen
+                     -1 -2  0  0 0;...  % oxygen
+                      0  0 -2  0 0];    % nitrogen
+                B = [-self.w;-self.x;(-self.y - 0.42*air_coeff);(-self.z - 1.58*air_coeff)];
 
-mol_handcalc = [B D E F]
+                mol_mat = linsolve(A,B);
+                mol_mat = [1; air_coeff; mol_mat*-1];
+                self.T_mat = [self.T_STP T_air 0 0 0 0 0]';
 
 
+                T_AFT = AFT(self, mol_mat, self.T_mat); 
+            %     T_AFT = 3189.37;                % Adiabatic Flame Temperature (K)
+            %     gamma_nzl = 1.3845;             % gamma at nozzle throat
+
+            elseif(phi < 1)                    
+            % ------------------------ Fuel lean Combustion ------------------------- % 
+            % C3.85 H4.85 N0.43 + (1/phi)(A)(0.71N2 + 0.2102) -> (B)H2O + (D)CO2 + (E)N2 + (F)O2
+            % ----------------------------------------------------------------------- % 
+                air_coeff = self.mol_st(1)/phi;
+                %     b  d  e  f g
+                A = [ 0 -1  0  0 0;...  % carbon
+                     -2  0  0  0 0;...  % hydrogen
+                     -1 -2  0 -2 0;...  % oxygen
+                      0  0 -2  0 0];    % nitrogen
+                B = [-self.w;-self.x;(-self.y - 0.42*air_coeff);(-self.z - 1.58*air_coeff)];
+
+                mol_mat = linsolve(A,B);
+                mol_mat = [1; air_coeff; mol_mat*-1];
+                self.T_mat = [self.T_STP T_air 0 0 0 0 0]';
 
 
-        
-        T_AFT = 3189.37;            % Adiabatic Flame Temperature (K)
-        gamma_nzl = 1.3845;         % gamma at nozzle throat        
-        
-    end 
+                T_AFT = AFT(self, mol_mat, self.T_mat);    
+            else   
 
-% Calculate heat of combustion
+            % ------------------------ Fuel Rich Combustion ------------------------- % 
+            % C3.85 H4.85 N0.43 + (1/phi)(A)(0.71N2 + 0.2102) -> (B)H2O + (D)CO2 + (E)N2 + (g)C3.85 H4.85 N0.43
+            % ----------------------------------------------------------------------- % 
+                air_coeff = self.mol_st(1)/phi;
+                %     b  d  e f  g
+                A = [ 0 -1  0 0 -3.85;...  % carbon
+                     -2  0  0 0 -4.85;...  % hydrogen
+                     -1 -2  0 0  0   ;...  % oxygen
+                      0  0 -2 0 -0.43];    % nitrogen
+                B = [-self.w;-self.x;(-self.y - 0.42*air_coeff);(-self.z - 1.58*air_coeff)];
 
-    reacttemp = 475; %K
-    molf = 1; %Moles of fuel (stoichiometric)
-    molair = 24.107;
-    molh2o = 2.425;
-    molco2 = 3.85;
-    moln2 = 19.26;
-    
-    molr = molf + molair;
-    molp = molh2o + molco2 + moln2;
-    
-    %Molecular weights needed (kg/kmol)
-    %Reactants
-    mwabs = 57.07;
-    mwair = 28.96;
-    
-    %Products
-    mwh2o = 18.02;
-    mwco2 = 44.01;
-    mwn2 = 28.013;
-    
-    %HEAT OF FORMATION FOR EACH MOLECULE%
+                mol_mat = linsolve(A,B);
+                mol_mat = [1; air_coeff; mol_mat*-1];
+                self.T_mat = [self.T_STP T_air 0 0 0 0 0]';
 
-    %hf(molecule) = heat of formation for each molecule
-    %Values found online
-
-    hfn2 = 0;
-    hfo2 = 0;
-    hfco2 = -393520; %kJ/kmol
-    hfh2o = -241820; %kJ/kmol
-    hfabs = 62630; %kJ/kmol
-
-    tstp = 273.15; %K
-
-    %Molar Cp of molecules needed (CP VALUES CHANGE WITH TEMPERATURE,WILL BE INCORPORATED AT A LATER TIME)
-
-    %Mass Cp values (kJ/kg*K)
-    %Other values taken at 300K
-    cpabs = 1.423; %Average value given, won't change with temperature
-    cpair = 1.006;
-    cph2o = 1.864;
-    cpco2 = .846;
-    cpn2 = 1.04;
-
-    %Molar Cp values
-    mcpabs = cpabs*mwabs;
-    mcpair = cpair*mwair;
-    mcph2o = cph2o*mwh2o;
-    mcpco2 = cpco2*mwco2;
-    mcpn2 = cpn2*mwn2;
-
-    %Mole Fractions
-    mfabs = molf/molr;
-    mfair = molair/molr;
-    mfh2o = molh2o/molp;
-    mfco2 = molco2/molp;
-    mfn2 = moln2/molp;
-
-    %Average Molar Cp values
-    mcpr = (mfabs * mcpabs) + (mfair * mcpair); %Reactants
-    mcpp = (mfh2o * mcph2o) + (mfco2 * mcpco2) + (mfn2 * mcpn2);
-
-    %HEAT OF COMBUSTION*
-
-    %Long equation will be simplified into terms to eliminate chances of parentheses errors.
-
-    %Followiing terms are number of moles multiplied by heat of formation
-    %HEATS OF FORMATION FOR N2 AND O2 ARE ZERO
-
-    %Products
-    h2oterm = molh2o * hfh2o;
-    co2term = molco2 * hfco2;
-
-
-    %Reactants
-    absterm = molf * hfabs;
-
-    %Heat of reaction determined, then multiplied by -1 to get heat of combustion.
-
-    molhrxn = h2oterm + co2term - absterm + (molp * mcpp * (reacttemp - tstp)) - (molr * mcpr * (reacttemp - tstp));
-
-    hrxn = molhrxn/mwabs;
-
-    hoc = hrxn * -1;
-
-    %fprintf("Heat of Combustion: %f kJ/kg\n", hoc);
-        
-
+                T_AFT = AFT(self, mol_mat, self.T_mat);     
+            end 
+        end
+    end
 end
+

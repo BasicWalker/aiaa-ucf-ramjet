3260341fe413ca75354bbe2748a21fe0e8e96c9c
BasicWalker <armalysamer1@gmail.com>
2021-03-26 17:50:04 -0400

    updated intake to increase clarity

--- combustion/SFRJ Internal Ballistics/Intake/Intake.m
+++ combustion/SFRJ Internal Ballistics/Intake/Intake.m
@@ -9,57 +9,68 @@
 % -----------------  --------  ---  ------------------------------
 % Durlak & Aubertin  01/22/21  ---  Initial Creation 
 % ---------------------------------------------------------------------- %
-    
-% Station 1
-[~, Ti_To1(n), Pi_Po1(n), Rho_Rho_o(n), ~] = flowisentropic(gamma, flight_mach(n));         % Isentropic flow properties at flight mach
-T_o1(n) = (1/Ti_To1(n)) * Temp_atm(n);                                                      % Stag Temp 1
-Po1(n) = (pressure_atm(n)/Pi_Po1(n));                                                       % Stag Pres 1
-Rho_o(n) = Rho_atm(n)/Rho_Rho_o(n);                                                         % Stag Density 1
-v_1(n) = flight_mach(n)*sqrt(gamma*R*Temp_atm(n));                                          % Velocity at station 1
+
+% Station definitions used for indices
+% station 1: free stream (ambient)
+% station 2: downstream of oblique shock, upstream of normal shock (external ramp)
+% station 3: downstream of normal shock, upstream of subsonic iscentropic expansion (throat)
+% station 4: downstream of subsonic iscentropic expansion combustor inlet,(combustion chamber opening)
+
+% Station 1 properties (free-stream)-----
+mach(1,n) = flight_mach(n);
+staticPres(1,n) = pressure_atm(n);  % <Pa>
+staticDens(1,n) = Rho_atm(n);  % <kg/m^3>   
+staticTemp(1,n) = Temp_atm(n);  % <K> 
+[~, tempRatio(1,n), presRatio(1,n), densRatio(1,n), ~] = flowisentropic(gamma, mach(1,n), 'mach');  % ratios are static over stagnation
+stagTemp(1,n) = staticTemp(1,n)/tempRatio(1,n);  % <K>                                                 
+stagPres(1,n) = staticPres(1,n)/presRatio(1,n);  % <Pa>                                                 
+stagDens(1,n) = staticDens(1,n)/densRatio(1,n);  % <kg/m^3>                                                   
+velocity(1,n) = mach(1,n)*sqrt(gamma*R*staticTemp(1,n));  % <m/s>                                          
          
-% Station 2 (oblique shock) 
-[mach_2(n), beta_angle(n)] = obliqueShock(flight_mach(n), def, gamma);                      % Oblique shock properties at flight mach
-mach_2_normal(n) = flight_mach(n)*sind(beta_angle(n));                                      % Normal component of mach after oblique shock
-[~, T2_To2(n), P2_Po2(n), Rho_Rho_o2(n), A_Astar_2(n)] = flowisentropic(gamma, mach_2(n));  % Isentropic flow properties at station 2 mach
-[~, T2_T1(n), ~, ~, ~, Po2_Po1(n), ~] = flownormalshock(gamma, mach_2_normal(n));           % Normal shock properties at mach2 normal component
-T_2(n) = T2_T1(n)*Temp_atm(n);                                                              % Static Temp 2
-Po_2(n) = Po2_Po1(n)*(1/Pi_Po1(n))*pressure_atm(n);                                         % Stag Pres 2
-P_2(n) = P2_Po2(n)*Po_2(n);                                                                 % Static Pres 2
-Rho_o_2(n) = Rho_o(n)*Po2_Po1(n);                                                           % Stag Density 2
-Rho_2(n) = Rho_Rho_o2(n)*Rho_o_2(n);                                                        % Static Density 2
-T_o2(n) = T_o1(n);                                                                          % Stag Temp 2
-v_2(n) = mach_2(n)*sqrt(gamma*R*T_2(n));                                                    % Velocity at station 2
+% Station 2 properties (oblique shock)----- 
+[mach(2,n), shockAngle(n)] = obliqueShock(mach(1,n), def, gamma);  % uniform mach number of external ramp region                      % Oblique shock properties at flight mach
+Mach1Norm(n) = mach(1,n)*sind(shockAngle(n));  % flow component crossing normal to oblique shock                                      % Normal component of mach after oblique shock
+[~, tempRatio(2,n), presRatio(2,n), densRatio(2,n), ~, stagPresRatio(2,n)] = ...
+    flownormalshock(gamma, Mach1Norm(n), 'mach');  % ratios are downstream over upstream
+stagPres(2,n) = stagPres(1,n) * stagPresRatio(2,n);  % <Pa>
+stagDens(2,n) = stagDens(1,n) * stagPresRatio(2,n);  % changes with the same rate as pressure (ideal gas law) <kg/m3>
+stagTemp(2,n) = stagTemp(1,n);  % does not change over normal shock <K>
+staticPres(2,n) = staticPres(1,n) * presRatio(2,n);  % <Pa>
+staticDens(2,n) = staticDens(1,n) * densRatio(2,n);  % <kg/m3>
+staticTemp(2,n) = staticTemp(1,n) * tempRatio(2,n);  % <K>
+velocity(2,n) = mach(2,n)*sqrt(gamma*R*staticTemp(2,n));  % <m/s>
+[~, ~, ~, ~, A_Astar(2,n)] = flowisentropic(gamma, mach(2,n), 'mach');
+Astar(2,n) = Area_intake/A_Astar(2,n);  % Area to choke before normal shock
         
-% Station 3 (normal shock)
-mach_3(n) = normalShock(mach_2(n), gamma);                                                  % Normal shock calc to solve for mach3
-[~, T3_T2(n), ~, ~, ~, Po3_Po2(n), ~] = flownormalshock(gamma, mach_2(n));                  % Normal shock properties at mach2
-[~, T3_To3(n), P3_Po3(n), Rho_Rho_o3(n), ~] = flowisentropic(gamma, mach_3(n));             % Isentropic flow properties at station 3 mach
-T_3(n) = T3_T2(n)*T2_T1(n)*Ti_To1(n)*T_o1(n);                                               % Static Temp 3
-To_3(n) = T_3(n) * (1/T3_To3(n));                                                           % Stag Temp 3
-Po_3(n) = Po3_Po2(n)*Po2_Po1(n)*(1/Pi_Po1(n))*pressure_atm(n);                              % Stag Pres 3
-P_3(n) = P3_Po3(n)*Po_3(n);                                                                 % Static Pres 3
-Rho_o_3(n) = Rho_o_2(n)*Po3_Po2(n);                                                         % Stag Density 3
-Rho_3(n) = Rho_Rho_o3(n)*Rho_o_3(n);                                                        % Static Density 3
-v_3(n) = mach_3(n)*sqrt(gamma*R*T_3(n));                                                    % Velocity at station 3
-MdotAir(n) = Rho_3(n)*Area_3*mach_3(n)*sqrt(gamma*R*T_3(n));                                % Mass flow rate
-        
-% Station 4 (combustor)
-Astar_2(n) = Area_3/A_Astar_2(n);                                                           % Area to choke before normal shock
-Astar_3(n) = Astar_2(n)/Po3_Po2(n);                                                         % Area to choke after nomal shock
-Acombustor_A3star(n) = Area_combustor/Astar_3(n);                                           % Area ratio of the combustor inlet
-[mach_4(n), T4_To4(n), P4_Po4(n), Rho_Rho_o4(n), ~] = flowisentropic(gamma, Acombustor_A3star(n), 'sub');
-To_4(n) =  To_3(n);                                                                         % Stag Temp 4
-Po_4(n) = Po_3(n);                                                                          % Stag Pres 4
-Rho_o_4(n) = Rho_o_3(n);                                                                    % Stag Density 4
-T_4(n) = T4_To4(n)*To_4(n);                                                                 % Static Temp 4
-P_4(n) = P4_Po4(n)*Po_4(n);                                                                 % Static Pres 4
-Rho_4(n) = Rho_Rho_o4(n)*Rho_o_4(n);                                                        % Static Density 4
-v_4(n) = mach_4(n)*sqrt(gamma*R*T_4(n));                                                    % Velocity at station 4
+% Station 3 properties (normal shock)-----
+[~, tempRatio(3,n), presRatio(3,n), densRatio(3,n), mach(3,n), stagPresRatio(3,n)] = ...
+    flownormalshock(gamma, mach(2,n), 'mach');  % ratios are downstream over upstream
+stagPres(3,n) = stagPres(2,n) * stagPresRatio(3,n);  % <Pa>
+stagDens(3,n) = stagDens(2,n) * stagPresRatio(3,n);  % changes with the same rate as pressure (ideal gas law) <kg/m3>
+stagTemp(3,n) = stagTemp(2,n);  % does not change over normal shock <K>
+staticPres(3,n) = staticPres(2,n) * presRatio(3,n);  % <Pa>
+staticDens(3,n) = staticDens(2,n) * densRatio(3,n);  % <kg/m3>
+staticTemp(3,n) = staticTemp(2,n) * tempRatio(3,n);  % <K>
+velocity(3,n) = mach(3,n)*sqrt(gamma*R*staticTemp(3,n));  % <m/s>
+MdotAir(n) = staticDens(3,n)*Area_intake*velocity(3,n);  % air mass flow at intake opening <kg/s>
+Astar(3,n) = Astar(2,n)/stagPresRatio(3,n);  % Area to choke after nomal shock
+A_Astar(3,n) = Area_combustor/Astar(3,n);  % Area ratio of the combustor inlet  
+
+% Station 4 properties (subsonic iscentropic expansion)-----  
+[mach(4,n), tempRatio(4,n), presRatio(4,n), densRatio(4,n), ~] = ...
+    flowisentropic(gamma, A_Astar(3,n), 'sub');  % ratios are static over stagnation
+stagPres(4,n) = stagPres(3,n);  % does not change; iscentropic <Pa>
+stagDens(4,n) = stagDens(3,n);  % does not change; iscentropic <kg/m3>
+stagTemp(4,n) = stagTemp(3,n);  % does not change; iscentropi <K>
+staticPres(4,n) = stagPres(4,n) * presRatio(4,n);  % <Pa>
+staticDens(4,n) = stagDens(4,n) * densRatio(4,n);  % <kg/m3>
+staticTemp(4,n) = stagTemp(4,n) * tempRatio(4,n);  % <K>
+velocity(4,n) = mach(4,n)*sqrt(gamma*R*staticTemp(4,n));  % <m/s>
 
 % Pass variables to ballistic simulator
-InltPres_stag(n) = Po_4(n);                                                                 % Inlet stagnation pressure
-InltPres(n) = P_4(n);                                                                       % Inlet static pressure
-InltRho(n) = Rho_4(n);                                                                      % Inlet static density
-InltTemp(n) = T_4(n);                                                                       % Inlet static temp
-InltVel(n) = v_4(n);                                                                        % Inlet velocity
-InltMach(n) = mach_4(n);                                                                    % Inlet mach number
+InltPres_stag(n) = stagPres(4,n);                                                                 % Inlet stagnation pressure
+InltPres(n) = staticPres(4,n);                                                                       % Inlet static pressure
+InltRho(n) = staticDens(4,n);                                                                      % Inlet static density
+InltTemp(n) = staticTemp(4,n);                                                                       % Inlet static temp
+InltVel(n) = velocity(4,n);                                                                        % Inlet velocity
+InltMach(n) = mach(4,n);                                                                    % Inlet mach number

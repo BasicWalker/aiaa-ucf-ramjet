c290b6def8bee670e0a8dec5c3384b485ffb7f3a
Sherlock-Ethan <Ethan.g.sherlock@gmail.com>
2021-04-15 10:15:07 -0400

    2DOF model update

--- Ramjet Simulator/FlightProfile/PlotData.m
+++ Ramjet Simulator/FlightProfile/PlotData.m
@@ -8,103 +8,141 @@
 % --------------  --------  ---  ------------------------------
 % Ethan Sherlock  01/22/21  000  Initial Creation 
 % Ethan Sherlock  02/14/21  001  Updated plots for SCR 001
-% Ethan Sherlock  02/14/21  005  1DOF trajectory update
-% Ethan Shelock   03/12/21  002  Updated plots for Chem Model
+% Ethan Sherlock  02/14/21  005  1DOF model update
+% Ethan Sherlock  03/12/21  002  Chem model update
+% Ethan Sherlock  04/14/21  ---  2DOF model update
 % ---------------------------------------------------------------------- %
-Thrustdlvd(n-1)     = 0.0;
-PC(n-1)             = 0.0;
-OFRatio(n-1)        = 0.0;
-InltPres(n-1)       = 0.0;
-PCreq(n-1)          = 0.0;
-PC_TAFT(n-1)        = 0.0;
-drag(n-1)           = 0.0;
-velocity(n-1)       = 0.0;
-acceleration(n-1)   = 0.0;
+thrust.F_t(n-1)         = trajectory.F_t(n-2);
+PC(n-1)                 = 0.0;
+OFRatio(n-1)            = 0.0;
+InltPres(n-1)           = 0.0;
+PCreq(n-1)              = 0.0;
+PC_TAFT(n-1)            = 0.0;
+trajectory.F_d(n-1)     = trajectory.F_d(n-2);
+trajectory.Vel(n-1)     = 0.0;
+trajectory.Acc(n-1)     = 0.0;
+trajectory.F_net(n-1)   = trajectory.F_net(n-2);
+MassFlow(n-1)           = 0.0;
+MdotAir(n-1)            = 0.0;
+MdotFuel(n-1)           = 0.0;
+InltPres_stag(n-1)      = 0.0;
+PCreq(n-1)              = 0.0;
+PC_TAFT(n-1)            = 0.0;
+phi_eqv(n-1)            = 0.0;
+T_stag(n-1)             = 0.0;
+gamma_t(n-1)            = 0.0;
+TotallImp(n-1)          = 0.0;
+
+fprintf('Simulation Complete \n')
 
 figure('Name','O/F Ratio')
-plot(BurnTime,OFRatio)
+plot(BurnTime(1:index-1),OFRatio(1:index-1))
 title('O/F Ratio')
 xlabel('Time (s)')
 ylabel('')
 grid on
+set(gcf,'position',[550,200,800,700])
 
 figure('Name','Force Profile')
-plot(BurnTime,Thrustdlvd2,BurnTime,drag, BurnTime, mass*gravity, BurnTime, (Thrustdlvd2 - mass*gravity - drag))
+plot(BurnTime,thrust.F_t,BurnTime,trajectory.F_d, BurnTime, Mass*gravity, BurnTime, trajectory.F_net)
 title('Thrust & Drag vs Time')
 xlabel('Time (s)')
 ylabel('Force (N)')
 legend('Thrust Curve','Drag','Weight','Net Force')
 grid on
+set(gcf,'position',[550,200,800,700])
 
 figure('Name','Mass Flow Rate')
-plot(BurnTime,MassFlow, BurnTime, MdotAir, BurnTime, MdotFuel) %
+plot(BurnTime(1:index-1),MassFlow(1:index-1), BurnTime(1:index-1), MdotAir(1:index-1), BurnTime(1:index-1), MdotFuel(1:index-1)) 
 title('Mass Flow Rate')
 xlabel('Time (s)')
 ylabel('Mass (kg/s)')
 legend('Total Mdot','Mdot Air','Mdot Fuel')
 grid on
-
-% figure('Name','Step Height')
-% plot(BurnTime,StepHeight)
-% title('Step Height')
-% xlabel('Time (s)')
-% ylabel('Height (m)')
-% grid on
-% ylim([0 .05])
+set(gcf,'position',[550,200,800,700])
 
 figure('Name','Pressure Plots')
-plot(BurnTime, InltPres_stag, BurnTime, PCreq, BurnTime, pressure_atm, BurnTime, PC_TAFT)
+plot(BurnTime(1:index-1), InltPres_stag(1:index-1), BurnTime(1:index-1), PCreq(1:index-1), BurnTime(1:index-1), pressure_a(1:index-1), BurnTime(1:index-1), PC_TAFT(1:index-1))
 title('Pressure Plots')
 xlabel('Time (s)')
 ylabel('Pressure (kPa)')
 legend('Inlet Pressure (stag)','Required Pressure - Choked Flow (stag)','Back Pressure (stag)', 'Chamber Pressure - Adiabatic Flame Temp (stag)')
 grid on
-
-figure('Name','Velocity & Acceleration Profiles')
-yyaxis left
-plot(BurnTime, velocity)
-title('Velocity and Acceleration Profiles')
-xlabel('Time (s)')
-ylabel('Velocity (m/s)')
-yyaxis right
-plot(BurnTime, acceleration)
-ylabel('Acceleration (m/s^2)')
-legend('Velocity','Acceleration')
-grid on
-
-figure('Name','Trajectory Profile')
-plot(BurnTime,altitude)
-title('Altitude vs Time')
-xlabel('Time (s)')
-ylabel('Altitude (m)')
-grid on
-ylim([0 max(altitude)+500])
+set(gcf,'position',[550,200,800,700])
 
 figure('Name','Flight Mach')
-plot(BurnTime,flight_mach)
+plot(BurnTime,Mach_f)
 title('Vehicle Mach vs Time')
 xlabel('Time (s)')
 ylabel('Mach No.')
 grid on
+set(gcf,'position',[550,200,800,700])
 
 figure('Name','Equivalence Ratio')
-plot(BurnTime,phi_eqv)
+plot(BurnTime(1:index-1),phi_eqv(1:index-1))
 title('Equivalence Ratio vs Time')
 xlabel('Time (s)')
 ylabel('Equivalence Ratio (phi)')
 grid on
-% ylim([0 16])
+% ylim([0 6])
+set(gcf,'position',[550,200,800,700])
 
 figure('Name','Adiabatic Flame Temperature')
 yyaxis left
-plot(BurnTime,T_stag)
+plot(BurnTime(1:index-1),T_stag(1:index-1))
 title('Adiabatic Flame Temperature & Specific Heat Ratio')
 xlabel('Time (s)')
 ylabel('Temperature <k>')
 yyaxis right
-plot(BurnTime,gamma_t)
+plot(BurnTime(1:index-1),gamma_t(1:index-1))
 ylabel('Specific Heat Ratio')
 grid on
+set(gcf,'position',[550,200,800,700])
+
+figure('Name','Trajectory Profile')
+axis equal
+subplot(3,1,1)
+plot(trajectory.X_pos,trajectory.Z_pos)
+title('Trajectory')
+xlabel('Range (m)')
+ylabel('Altitude (m)')
+grid on
+subplot(3,1,2)
+plot(BurnTime,trajectory.Z_pos)
+title('Altitude vs Time')
+xlabel('Time(s)')
+ylabel('Altitude(m)')
+grid on
+subplot(3,1,3)
+plot(BurnTime,trajectory.X_pos)
+title('Range vs Time')
+xlabel('Time(s)')
+ylabel('Range(m)')
+grid on
+set(gcf,'position',[550,200,800,700])
+
+figure('Name','Velocity/Acceleration Profile')
+axis equal
+subplot(2,1,1)
+grid on
+title('Vertical Profile (Z)')
+yyaxis left
+plot(BurnTime, trajectory.Vel_z)
+ylabel('Vz (m/s)')
+yyaxis right
+plot(BurnTime, trajectory.Acc_z)
+ylabel('Az (m/s^2)')
+subplot(2,1,2)
+grid on;
+title('Horizontal Profile (X)')
+yyaxis left
+plot(BurnTime, trajectory.Vel_x)
+xlabel('Time (s)')
+ylabel('Vx (m/s)')
+yyaxis right
+plot(BurnTime, trajectory.Acc_x)
+ylabel('Ax (m/s^2)')
+set(gcf,'position',[550,200,800,700])
 
 if (mean(InltMach) > 0.2)
     fprintf(2,'\nWARNING: Inlet Mach number is too high.\n')
@@ -112,24 +150,23 @@ end
 if (mean(phi_eqv) > 3.0)
     fprintf(2,'WARNING: Equivalence ratio exceeds flamability limit.\n')
 end
-if (flight_mach(n-1) < 2)
+if (Mach_f(n-1) < 2)
     fprintf(2,'WARNING: Vehicle does not reach target Mach Number.\n')
 end
 
 fprintf('------------ Simulation Results ------------\n')
 fprintf('Burn Time:                  %.2f    (s)\n', BurnTime(n-1))
-fprintf('Average Thrust:             %.2f   (N)\n', mean(Thrustdlvd))
-fprintf('Average Drag Force:         %.2f   (N)\n',mean(drag))
-fprintf('Total Impulse:              %.2f  (Ns)\n', TotallImp(n-1))
+fprintf('Average Thrust:             %.2f   (N)\n', mean(trajectory.F_t))
+fprintf('Average Drag Force:         %.2f   (N)\n',mean(trajectory.F_d))
+fprintf('Total Impulse:              %.2f  (Ns)\n', max(TotallImp))
 fprintf('Air Mass Flow Rate:         %.3f    (kg/s)\n', mean(MdotAir))
-fprintf('PC From TAFT:               %.2f  (kPa)\n',mean(PC_TAFT))
+fprintf('Chamber Pressure From TAFT: %.2f   (kPa)\n',mean(PC_TAFT))
 fprintf('Initial Step Height:        %.2f     (in) \n', StepHeight(1)*In2Mtr)
 fprintf('Average Inlet Velocity:     %.2f   (m/s)\n', mean(InltVel))
-fprintf('Max Altitude(During Boost): %.2f (m)\n',max(altitude))
+fprintf('Max Altitude(During Boost): %.2f (m)\n',max(trajectory.Z_pos))
 fprintf('Average Inlet Mach:         %.2f  \n', mean(InltMach))
 fprintf('Average O/F Ratio:          %.2f  \n',mean(OFRatio))
 fprintf('Average Equivalence Ratio:  %.2f  \n',mean(phi_eqv))
 fprintf('Average Gamma Ratio:        %.2f  \n',mean(gamma_t))
 fprintf('--------------------------------------------\n')
-
-
+toc
